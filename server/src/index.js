64699eb2717a167d46ab59bc8f5e106996f101aa
kenng90 <kenngigz@yahoo.com>
2026-08-31 13:16:02 +0300

    feat(server): run drop-expiry job on API startup

--- server/src/index.js
+++ server/src/index.js
@@ -2,9 +2,11 @@ import 'dotenv/config';
 import { createServer } from 'http';
 import { createApp } from './app.js';
 import { env } from './config/env.js';
+import { startJobs } from './jobs/expireDrops.js';
 
 const app = createApp();
 const server = createServer(app);
+startJobs();
 
 server.listen(env.port, () => {
   console.log(`ShiftSync API listening on port ${env.port}`);
