dd65b00cdbf4fab728255d0299b80a1c6cf845e3
kenng90 <kenngigz@yahoo.com>
2026-08-31 14:10:05 +0300

    feat(realtime): attach Socket.io to the HTTP server

--- server/src/index.js
+++ server/src/index.js
@@ -3,9 +3,11 @@ import { createServer } from 'http';
 import { createApp } from './app.js';
 import { env } from './config/env.js';
 import { startJobs } from './jobs/expireDrops.js';
+import { attachSockets } from './sockets.js';
 
 const app = createApp();
 const server = createServer(app);
+attachSockets(server);
 startJobs();
 
 server.listen(env.port, () => {
